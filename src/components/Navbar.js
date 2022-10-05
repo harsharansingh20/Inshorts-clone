07933e87c1e45e3ef7e0171c6c12841652c46ed9
harsharan  Singh <harsharansingh20@gmail.com>
2022-10-05 21:13:09 +0530

    menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Navbar.css";
+import HamburgerDrawer from "./HamburgerDrawer";
+
+
+
+const Navbar = () => {
+  return (
+    <>
+    
+    <div className='nav'>
+       <div className="icon">
+        <HamburgerDrawer/>
+       </div>
+
+      <img id='navlogo' src="https://assets.inshorts.com/website_assets/images/logo_inshorts.png"  alt="" />
+
+    </div>
+    </>
+  )
+}
+
+export default Navbar
